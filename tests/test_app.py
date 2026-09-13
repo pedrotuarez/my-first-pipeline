2f850b7aa7289db1c52b6d4815df98340405f919
Pedro Tuarez <pedrotuarez.g@gmail.com>
2026-09-13 18:40:37 -0400

    Add Funcion Multiplicar y Unit Tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,15 @@
 import unittest
 
-from src.app import sumar
+from src.app import sumar, multiplicar
 
 
 class TestApp(unittest.TestCase):
 
     def test_suma(self):
         self.assertEqual(sumar(2, 3), 5)
+
+    def test_multiplicacion(self):
+        self.assertEqual(multiplicar(2, 3), 6)
 
 
 if __name__ == "__main__":
